9fff2a260dc7537e80e957a4c5256da5e78a8128
lifeilong <lifeilong@togeek.cn>
2018-10-11 08:04:26 +0800

    ku

--- config/k8s-image.sh
+++ config/k8s-image.sh
@@ -9,3 +9,5 @@ for im in ${images[@]};do
     $dk tag $name $im
     $dk rmi $name
 done
+
+kubeadm join 192.168.137.200:6443 --token cl60h7.y4o6vpizk64j5cz6 --discovery-token-ca-cert-hash sha256:1fda5b34ecd3abb044ff137e5e74afbeea95cfdb881038ed78961b8d2ef00017
